5931852c452fb1881dbf1e0941dc327d9584996e
Mr-Raja12 <rk9576899082@gmail.com>
2022-12-03 01:50:50 +0530

    Added: Edited existing contact person using their name

--- src/com/bl/addressbook/AddressBook.java
+++ src/com/bl/addressbook/AddressBook.java
@@ -1,7 +1,9 @@
 package com.bl.addressbook;
+import java.util.ArrayList;
 import java.util.Scanner;
 public class AddressBook {
     Scanner sc = new Scanner(System.in);
+    ArrayList<ContactPerson> addressBook1 = new ArrayList<>();
     /**
      *Creating a new contact
      */
@@ -33,5 +35,46 @@ public class AddressBook {
         ContactPerson contactPerson = createContact();
         System.out.println(contactPerson);
         System.out.println("Contact added successfully");
+    }
+    /**
+     * Creating a New contact from existing contact
+     */
+    public void editContact() {
+        boolean isContactFound = false;
+        System.out.println("Enter Name to edit Contact");
+        String name = sc.next();
+        for (ContactPerson contactPerson : addressBook1) {
+            if (name.equalsIgnoreCase(contactPerson.getFirstName())) {
+                isContactFound = true;
+                System.out.print("Enter First Name :");
+                contactPerson.setFirstName(sc.next());
+                System.out.print("Enter Last Name :");
+                contactPerson.setLastName(sc.next());
+                System.out.print("Enter Address :");
+                contactPerson.setAddress(sc.next());
+                System.out.print("Enter City :");
+                contactPerson.setCity(sc.next());
+                System.out.print("Enter State :");
+                contactPerson.setState(sc.next());
+                System.out.print("Enter email :");
+                contactPerson.setEmail(sc.next());
+                System.out.print("Enter ZipCode :");
+                contactPerson.setZipCode(sc.nextInt());
+                System.out.print("Enter Phone Number :");
+                contactPerson.setPhoneNumber(sc.nextLong());
+                System.out.println(contactPerson);
+                break;
+            }
+        }
+        if (isContactFound) {
+            System.out.println("Contact Updated Successfully..");
+        } else {
+            System.out.println("Oops...Contact not found");
+        }
+    }
+    void display(){
+        for (ContactPerson person : addressBook1) {
+            System.out.println(person);
+        }
     }
 }
