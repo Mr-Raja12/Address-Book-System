9b97fd053a84bd8dfe7c88f135441779c7861e74
Mr-Raja12 <rk9576899082@gmail.com>
2022-12-02 13:30:51 +0530

    Welcome to Address Book System

---
+++ src/com/bl/addressbook/AddressBookMain.java
@@ -0,0 +1,10 @@
+package com.bl.addressbook;
+public class AddressBookMain {
+        /**
+         * @param Raja baitha
+         */
+        public static void main(String[] args) {
+            System.out.println("Welcome to address book Program");
+        }
+}
+
